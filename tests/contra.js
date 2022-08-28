29ec0a516d81e9c29c758bad3163c01d4fbfd690
Alvaro Devesa <alvdev@outlook.com>
2022-08-28 16:33:09 +0200

    Replace code for a working one

--- tests/contra.js
+++ tests/contra.js
@@ -1,13 +1,31 @@
-const playwright = require('playwright');
-async function main() {
-  const browser = await playwright.chromium.launch({
-    headless: false,
+const playwright = require('@playwright/test');
+const data = [];
+
+(async () => {
+  const browser = await playwright['chromium'].launch({
+    headless: true,
+    args: ['--no-sandbox'],
   });
-}
 
-const page = await browser.newPage();
-await page.goto('https://finance.yahoo.com/world-indices');
-await page.waitForTimeout(5000);
-await browser.close();
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-main();
+  await page.goto('https://old.reddit.com/r/askreddit');
+  await page.waitForSelector('#siteTable');
+
+  const posts = await page.$$('div.thing');
+
+  for (let post of posts) {
+    const title = await post.$eval('.title a', el => el.textContent);
+    const url = await post.$eval('.title a', el => el.href);
+    const upvotes = await post.$eval('.score.unvoted', el => el.textContent);
+    const comments = await post.$eval('.comments', el => el.textContent);
+    const time = await post.$eval('.tagline time', el => el.textContent);
+    data.push({ title, url, upvotes, comments, time });
+  }
+
+  console.log(data);
+  console.log(data.length);
+
+  await browser.close();
+})();
