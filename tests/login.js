a3a9936c702703bf842f20d6d993820be46013ed
Alvaro Devesa <alvdev@outlook.com>
2022-08-31 13:30:14 +0200

    Add config module

--- tests/login.js
+++ tests/login.js
@@ -1,4 +1,5 @@
-const { chromium } = require('@playwright/test');
+import { chromium } from '@playwright/test';
+import { login } from './config.js';
 
 (async () => {
   const browser = await chromium.launch({
@@ -6,10 +7,10 @@ const { chromium } = require('@playwright/test');
   });
   const page = await browser.newPage();
 
-  await page.goto('https://www.contradefensa.com/ghost');
+  await page.goto(login.url);
 
-  await page.type('input[name="identification"]', 'myemail');
-  await page.type('input[name="password"]', 'mypassword');
+  await page.type('input[name="identification"]', login.email);
+  await page.type('input[name="password"]', login.password);
   await page.keyboard.press('Enter');
 
   await page.waitForTimeout(10000);
